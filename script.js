4b1c1f0d3722a5c013d5e6c1da3cf23d35d0a301
aismeg <iaskja20@gmai.com>
2022-10-05 20:41:12 +0300

    gamma

--- script.js
+++ script.js
@@ -2,31 +2,17 @@
 
 
 const title = prompt('Как называется ваш проект?');
-
-
 const screens = prompt('Какие типы экранов нужно разработать?');
-
-
-
 const screenPrice = +prompt('Сколько будет стоить данная работа?');
-parseInt(screenPrice);
-
-
-let adaptive = prompt('Нужен ли адаптив на сайте?');
+const adaptive = prompt('Нужен ли адаптив на сайте?');
 
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
-
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
-
 const servicePrice1 = +prompt('Сколько это будет стоить?');
-parseInt(servicePrice1);
-
 const servicePrice2 = +prompt('Сколько это будет стоить?');
-parseInt(servicePrice2);
 
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-const servicePercentPrice = Math.ceil(fullPrice);
+const servicePercentPrice = Math.ceil(fullPrice - (Math.random() * 99 / 100));
 console.log(servicePercentPrice + ' рублей');
 
 if (fullPrice > 30000) {
