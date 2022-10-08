733c4b1259d0de62cf1cbcb1a34ac18ac82aee51
aismeg <iaskja20@gmai.com>
2022-10-08 08:27:58 +0300

    improved

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 'use strict';
 
 
-const title = prompt('Как называется ваш проект?');
-console.log(title);
-const screens = prompt('Какие типы экранов нужно разработать?');
-console.log(screens);
-const screenPrice = +prompt('Сколько будет стоить данная работа?');
-console.log(parseInt(screenPrice));
-const adaptive = prompt('Нужен ли адаптив на сайте?');
-console.log(Boolean(adaptive));
-
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-console.log(service1);
-const servicePrice1 = +prompt('Сколько это будет стоить?');
-console.log(parseInt(servicePrice1));
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-console.log(service2);
-const servicePrice2 = +prompt('Сколько это будет стоить?');
-console.log(parseInt(servicePrice2));
-
 let rollback = Math.random() * 99;
+
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?');
+const screenPrice = +prompt('Сколько будет стоить данная работа?');
+const adaptive = prompt('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
 
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 const servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
-console.log(Math.ceil(servicePercentPrice) + ' рублей');
+
 
 if (fullPrice > 30000) {
     console.log('Даем скидку в 10%');
@@ -34,6 +25,19 @@ if (fullPrice > 30000) {
 } else {
     console.log('Что-то пошло не так');
 }
+
+console.log(title);
+console.log(screens);
+console.log(parseInt(screenPrice));
+
+console.log(Boolean(adaptive));
+
+console.log(service1);
+console.log(parseInt(servicePrice1));
+console.log(service2);
+console.log(parseInt(servicePrice2));
+
+console.log(Math.ceil(servicePercentPrice) + ' рублей');
 
 
 
